04e5409b89b2932af85d7056b2940571b9a76134
Hugo Baeta <hugo@baeta.me>
2014-10-30 14:11:34 -0700

    Initial file structure commit

---
+++ www/js/sketch.js
@@ -0,0 +1,18 @@
+// Academy of Art University
+// WNM 498 Generative Art & Creative Code
+//
+// Rite of Spring - Changing of the Seasons Interactive Story
+//
+// Hugo Baeta  [hugobaeta.com]
+// Tonson Pine
+
+
+
+function setup(){
+  createCanvas( windowWidth, windowHeight );
+
+}
+
+function draw(){
+
+}
