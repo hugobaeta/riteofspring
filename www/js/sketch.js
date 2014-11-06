1079ebcedab2f924a1df3bc1753b10bf43e91cb0
Hugo Baeta <hugo@baeta.me>
2014-11-06 14:17:23 -0800

    Added new illustrator files with separated scene, updated other illustrations, and added SVG to html

--- www/js/sketch.js
+++ www/js/sketch.js
@@ -6,38 +6,17 @@
 // Hugo Baeta  [hugobaeta.com]
 // Tonson Pine
 
-var img,
-		img2,
-		img3,
-		img4;
-var tint;
-var x;
 
+//Variables
+
+
+// Setup
 function setup() {
-	// noCanvas();
-  createCanvas( windowWidth, windowHeight );
+	noCanvas();
 }
 
+// Draw
 function draw() {
 	background(190, 245, 250)
-
-  // // Displays the image at its actual size at point (0,0)
-  // image(img, x, -20, 550, 180);
-  // // // Displays the image at point (0, height/2) at half size
-  // // image(img, 0, 0, img.width/2, img.height/2);
-	//
-	//
-  // if (x > windowWidth*0.35) {
-  //   x = x;
-  //   if(tint > 255){
-	//
-  // 	}else{
-  // 		tint(255, tint);
-  // 		tint = tint+10;
-	//
-  // 	}
-  // }else{
-  // 	x = x + 5;
-  // }
 
 }
